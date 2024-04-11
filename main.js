0e8ecc832357a2c11b9d93cd8ac818640759fe1b
Natan Rodrigues <139073468+natan-rs@users.noreply.github.com>
2024-04-11 13:21:25 -0300

    main.js

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 // Variáveis
-
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
 let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
 
-// função callback
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+
+
+// Funções 
 function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector('#inputNumber')
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add('hide')
-    screen2.classList.remove('hide')
-
+    toggleScreen()
     document.querySelector('.screen2 h2').innerText = `Acertou em ${xAttempts} tentativas`
   }
 
@@ -23,14 +28,12 @@ function handleTryClick(event) {
   xAttempts++
 }
 
-
-// Eventos
-const btnTry = document.querySelector('#btnTry')
-const btnReset = document.querySelector('#btnReset')
-
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function() {
+function handleResetClick() {
   window.location.reload(true)
-
   xAttempts = 1
-})
+}
+
+function toggleScreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
